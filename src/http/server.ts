547fa43af6748afacc9e4a696830ee79cf1eb704
ArthurRod <arthurrod.dev@gmail.com>
2024-02-10 23:39:01 -0300

    Create votes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,13 +1,20 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { createPoll } from "./routes/create-polls";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
+const app = fastify();
 
-const app = fastify()
+app.register(cookie, {
+  secret: "secret-polls-nlw-nodejs",
+  hook: "onRequest",
+});
 
-app.register(createPoll)
-app.register(getPoll)
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP server running!')
-})
+  console.log("HTTP server running!");
+});
